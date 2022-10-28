7ac47d1ee9c16a3cee94c2378ff008d662c7cbb9
Wesley Sardi <wesley.sardi@gmail.com>
2022-10-28 13:31:21 -0300

    Parte 2 finalizada.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:tempo_template/services/location.dart';
+import 'package:tempo_template/services/networking.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+
+import '../services/weather.dart';
+import 'location_screen.dart';
+
+const apiKey = 'c7e803ecb444bf521ed5e2b74a9f7aa6';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,16 +20,42 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
+  void pushToLocationScreen(dynamic weatherData) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(localWeatherData: weatherData);
+    }));
+  }
+
+  void getData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+    pushToLocationScreen(weatherData);
+  }
+
+  Future<void> getLocation() async {
+    var location = Location();
+    await location.getCurrentLocation();
+
+    latitude = location.latitude!;
+    longitude = location.longitude!;
+
+    getData();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
-          child: const Text('Obter Localização'),
-        ),
+    return const Center(
+      child: SpinKitDoubleBounce(
+        color: Colors.white,
+        size: 100.0,
       ),
     );
   }
